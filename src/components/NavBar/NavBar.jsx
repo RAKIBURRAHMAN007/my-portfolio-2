89cf9dd26746987bf2e2ebc7f972374ca7c308d0
RAKIBURRAHMAN007 <ratulrakibur5@gmail.com>
2025-01-04 15:43:42 +0600

    banner page complete

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,71 @@
+
+import { Link, NavLink } from 'react-router-dom';
+import logo from '../../assets/logo.png'
+
+const NavBar = () => {
+
+    const links = <>
+        <li><NavLink to='/'>Home</NavLink></li>
+        <li><NavLink to='/learning'>Start-Learning</NavLink></li>
+        <li><NavLink to='/tutorials'>Tutorials</NavLink></li>
+        <li><NavLink to='/aboutUs'>About-Us</NavLink></li>
+
+
+    </>
+    return (
+
+        <div className="navbar fixed z-30 bg-white bg-opacity-75">
+            <div className="navbar-start ">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 6h16M4 12h8m-8 6h16" />
+                        </svg>
+                    </div>
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content bg-gray-200  rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                        {
+                            links
+                        }
+                    </ul>
+                </div>
+                <div className="btn bg-[#e4e4e4]">
+                    <img className='w-20' src={logo} alt="" />
+                </div>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1 text-xl ">
+                    {
+                        links
+                    }
+                </ul>
+            </div>
+            <div className="navbar-end">
+
+                <button className="btn btn-outline border-0 border-l-2 border-b-8 border-r-4 border-t-2 md:w-48">
+                    Download Resume
+                </button>
+
+
+
+
+            </div>
+
+
+
+        </div>
+
+    );
+};
+
+export default NavBar;
---
+++ src/pages/Root/Root.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import NavBar from '../../components/NavBar/NavBar';
+import { Outlet } from 'react-router-dom';
+
+const Root = () => {
+    return (
+        <div className="min-h-screen bg-[#f5f5f5] flex flex-col justify-between">
+        <div className=''>
+            <NavBar></NavBar>
+            <Outlet></Outlet>
+           
+        </div>
+    </div>
+    );
+};
+
+export default Root;
