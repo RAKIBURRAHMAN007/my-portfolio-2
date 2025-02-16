d9c8e326605a392639f4316f6eccfb5a9e8fed37
RAKIBURRAHMAN007 <ratulrakibur5@gmail.com>
2025-02-16 22:05:47 +0600

    helmet update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,29 +1,27 @@
-import React from 'react';
-import Banner from '../../components/Banner/Banner';
-import AboutMe from '../../components/AboutMe/AboutMe';
-import FeaturedMe from '../../components/FeatureMe/FeatureMe';
-import Myskills from '../../components/Skills/Myskills';
-import MyEducation from '../../components/MyEducation/MYEducation';
-import EducationSmall from '../../components/EducationSmall/EducationSmall';
-import MyLatestProjects from '../../components/MyLatestProjects/MyLatestProjects';
-import { Helmet } from 'react-helmet';
+import React from "react";
+import Banner from "../../components/Banner/Banner";
+import AboutMe from "../../components/AboutMe/AboutMe";
+import FeaturedMe from "../../components/FeatureMe/FeatureMe";
+import Myskills from "../../components/Skills/Myskills";
+import MyEducation from "../../components/MyEducation/MYEducation";
+import EducationSmall from "../../components/EducationSmall/EducationSmall";
+import MyLatestProjects from "../../components/MyLatestProjects/MyLatestProjects";
+import { Helmet } from "react-helmet";
 
 const Home = () => {
-    return (
-        <div>
-            <Helmet>
-                <meta charSet="utf-8" />
-                <title>Home | Ratul-Portfolio</title>
-                
-            </Helmet>
-            <Banner></Banner>
-            <FeaturedMe></FeaturedMe>
-            <Myskills></Myskills>
-            <EducationSmall></EducationSmall>
-            <MyLatestProjects></MyLatestProjects>
-
-        </div>
-    );
+  return (
+    <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Home | Ratul-Portfolio</title>
+      </Helmet>
+      <Banner></Banner>
+      <FeaturedMe></FeaturedMe>
+      <Myskills></Myskills>
+      <EducationSmall></EducationSmall>
+      <MyLatestProjects></MyLatestProjects>
+    </div>
+  );
 };
 
 export default Home;
